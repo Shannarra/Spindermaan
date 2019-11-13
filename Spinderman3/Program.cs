cec98a55d59384f0a91dc18256a70060390c533c
Shannarra <petarangelov15@gmail.com>
2019-11-13 06:58:39 -0800

    Cleared a bug

--- Spinderman3/Program.cs
+++ Spinderman3/Program.cs
@@ -16,6 +16,28 @@ namespace Spinderman3
             Console.Write("File to read: ");
             var file = Console.ReadLine();
 
+           
+            try
+            {
+                 using (StreamReader reader = new StreamReader(file))
+                                while(!reader.EndOfStream)
+                                    coll.Add(reader.ReadLine());
+            }
+            catch (FileNotFoundException) 
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"The file \"{file}\" cannot be found or does not exist!");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Main();
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Access to \"{file}\" denied. Please try again.");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Main();
+            }
+            
             Console.Write("Prefered name of the .spindermaan file [filif.spindermaan]:" );
             var oname = Console.ReadLine();
             string name = "";
@@ -25,9 +47,7 @@ namespace Spinderman3
             else
                 name = $"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}" + @"\" + oname + ".spindermaan";
 
-            using (StreamReader reader = new StreamReader(file))
-                while(!reader.EndOfStream)
-                    coll.Add(reader.ReadLine());
+           
 
             Parser.Run(coll, name);
 
